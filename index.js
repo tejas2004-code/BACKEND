8d4d550523e091e71901e15c896816b1c7f74290
tejas2004-code <tejasghadigaonkar02@gmail.com>
2025-05-14 15:47:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({ extended: true }))
 
 
+app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-gamma-five-43.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 
 app.use(express.json())
 app.use(cors({
