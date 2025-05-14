6f7bd51c8dea8d54853e78b8ddf69080d44029b3
tejas2004-code <tejasghadigaonkar02@gmail.com>
2025-05-14 15:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+    origin: 'https://frontend-gamma-five-43.vercel.app',
+    credentials: true
+}));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 // app.get('/', function (req, res) {
